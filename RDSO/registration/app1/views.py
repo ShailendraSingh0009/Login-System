e4619d03c8bd237f9367fb8752e007ad9d2ea19e
Shailendra Singh <119068680+ShailendraSingh0009@users.noreply.github.com>
2023-06-23 12:36:37 +0530

    Add files via upload

---
+++ RDSO/registration/app1/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
+def signupPage(request):
+    if request.method=="POST":
+        uname=request.POST.get('username')
+        eml=request.POST.get('email')
+        pass1=request.POST.get('password')
+        pass2=request.POST.get('confirmpassword')
+        #print(uname,eml,pass1)
+        if pass1==pass2:
+            if User.objects.filter(username=uname).exists():
+                return HttpResponse("Username already taken!")
+            elif User.objects.filter(email=eml).exists():
+                return HttpResponse("Email already taken!")
+            else:
+                my_user=User.objects.create_user(uname,eml,pass1)
+                my_user.save()
+        else:
+            return HttpResponse("Password not matching!")
+        return redirect('login')
+        #return HttpResponse("user created successfully!")
+    return render(request,'signup.html')
+
+def loginPage(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("Invalid username or password!")
+    return render(request,'login.html')
+
+def homePage(request):
+    return render(request,'home.html')
+
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
+# Create your views here.
